b92c3f113016b969d6e8bea793ca4acaf9e516a8
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-05-05 10:58:43 +0500

    Initial commit

---
+++ components/componentsindex.js
@@ -0,0 +1,29 @@
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+import NavBar from "./NavBar/NavBar";
+import HeroSection from "./HeroSection/HeroSection";
+import Service from "./Service/Service";
+import BigNFTSilder from "./BigNFTSilder/BigNFTSilder";
+import Subscribe from "./Subscribe/Subscribe";
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+import NFTCard from "./NFTCard/NFTCard";
+import Collection from "./Collection/Collection";
+import FollowerTab from "./FollowerTab/FollowerTab";
+import Title from "./Title/Title";
+
+export {
+  Button,
+  Footer,
+  NavBar,
+  HeroSection,
+  Service,
+  BigNFTSilder,
+  Subscribe,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+  FollowerTab,
+  Title,
+};
---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+  Subscribe,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+  FollowerTab,
+  Title,
+} from "../components/componentsindex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+      <FollowerTab />
+      <Collection />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
+    </div>
+  );
+};
+
+export default Home;
