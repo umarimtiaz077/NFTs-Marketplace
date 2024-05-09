575ce6274c23486f6d05b45926f3549c285f2af4
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-05-09 09:27:44 +0500

    Upload NFT Page

--- components/NavBar/Discover/Discover.jsx
+++ components/NavBar/Discover/Discover.jsx
@@ -17,7 +17,7 @@ const Discover = () => {
     },
     {
       name: "Author Profile",
-      link: "author-profile",
+      link: "author",
     },
     {
       name: "NFT Details",
@@ -26,6 +26,10 @@ const Discover = () => {
     {
       name: "Account Setting",
       link: "account",
+    },
+    {
+      name: "Upload NFT",
+      link: "uploadNFT",
     },
     {
       name: "Connect Wallet",
