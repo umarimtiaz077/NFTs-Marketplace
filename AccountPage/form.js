f0a885fdd3b6b2e46da670f2035b9bba33b830d0
Malik-Haider <27709@students.riphah.edu.pk>
2024-11-06 15:42:47 +0500

    AccountPage Component

---
+++ AccountPage/form.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { HiOutlineMail } from "react-icons/hi";
+import { MdOutlineHttp, MdOutlineContentCopy } from "react-icons/md";
+import {
+  TiSocialFacebook,
+  TiSocialTwitter,
+  TiSocialInstagram,
+} from "react-icons/ti";
+
+//INTERNAL IMPORT
+import Style from "./Form.module.css";
+import { Button } from "../../components/componentsindex.js";
+
+const Form = () => {
+  return (
+    <div className={Style.Form}>
+      <div className={Style.Form_box}>
+        <form>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="name">Username</label>
+            <input
+              type="text"
+              placeholder="shoaib bhai"
+              className={Style.Form_box_input_userName}
+            />
+          </div>
+
+         
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
