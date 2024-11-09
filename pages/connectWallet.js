77d7f6aade57c9e44d6c60d98a88343a3a0e50b1
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-09 17:02:48 +0500

    connect wallet

---
+++ pages/connectWallet.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useContext } from "react";
+import Image from "next/image";
+
+//INTERNAL IMPORT
+import Style from "../styles/connectWallet.module.css";
+import images from "../img";
+
+//IMPORT FROM SMART CONTRACT
+import { NFTMarketplaceContext } from "../Context/NFTMarketplaceContext";
+const connectWallet = () => {
+  const [activeBtn, setActiveBtn] = useState(1);
+  const { currentAccount, connectWallet } = useContext(NFTMarketplaceContext);
+  const providerArray = [
+    {
+      provider: images.provider1,
+      name: "Metamask",
+    },
+    {
+      provider: images.provider2,
+      name: "walletConnect",
+    },
+    {
+      provider: images.provider3,
+      name: "walletlink",
+    },
+    {
+      provider: images.provider1,
+      name: "Formatic",
+    },
+  ];
+  return (
+    <div className={Style.connectWallet}>
+      <div className={Style.connectWallet_box}>
+        <h1>Connect your wallet</h1>
+        <p className={Style.connectWallet_box_para}>
+          Connect with one of our avaliabl wallet providers or create a new one
+        </p>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default connectWallet;
