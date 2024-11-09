4ecfb007b35c2dc3196d32a4329d07c8fa99220b
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-09 10:06:53 +0500

    Network Setup(polygon-mainnet)

--- Context/constants.js
+++ Context/constants.js
@@ -35,4 +35,15 @@ const networks = {
     rpcUrls: ["https://rpc.ankr.com/polygon_mumbai"],
     blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
   },
+  polygon: {
+    chainId: `0x${Number(137).toString(16)}`,
+    chainName: "Polygon Mainnet",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc.ankr.com/polygon"],
+    blockExplorerUrls: ["https://polygonscan.com/"],
+  },
 }
