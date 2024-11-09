55fdf3338a0e84d9411599ba9677467a2113fe87
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-09 09:44:16 +0500

    about.js

---
+++ pages/aboutus.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+
+//INTERNAL IMPORT
+import Style from "../styles/aboutus.module.css";
+import { Brand } from "../components/componentsindex";
+import images from "../img";
+
+const aboutus = () => {
+  const founderArray = [
+    {
+      name: "Niamh O'Shea",
+      position: "Co-founder and Chief Executive",
+      images: images.founder1,
+    },
+    {
+      name: "Danien Jame",
+      position: "Co-founder and Chief Executive",
+      images: images.founder2,
+    },
+    {
+      name: "Orla Dwyer",
+      position: "Co-founder, Chairman",
+      images: images.founder3,
+    },
+    {
+      name: "Dara Frazier",
+      position: "Co-Founder, Chief Strategy Officer",
+      images: images.founder4,
+    },
+  ];
+
+  const factsArray = [
+    {
+      title: "10 million",
+      info: "Articles have been public around the world (as of Sept. 30, 2021)",
+    },
+    {
+      title: "100,000",
+      info: "Registered users account (as of Sept. 30, 2021)",
+    },
+    {
+      title: "220+",
+      info: "Countries and regions have our presence (as of Sept. 30, 2021",
+    },
+  ];
+  return (
+    <div className={Style.aboutus}>
+      <div className={Style.aboutus_box}>
+        <div className={Style.aboutus_box_hero}>
+          <div className={Style.aboutus_box_hero_left}>
+            <h1>👋 About Us.</h1>
+            <p>
+              We’re impartial and independent, and every day we create
+              distinctive, world-class programmes and content which inform,
+              educate and entertain millions of people in the around the world.
+            </p>
+          </div>
+          <div className={Style.aboutus_box_hero_right}>
+            <Image src={images.hero2} />
+          </div>
+        </div>
+
+        
+      </div>
+      <Brand />
+    </div>
+  );
+};
+
+export default aboutus;
