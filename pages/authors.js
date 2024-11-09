d4c6ca99cb16ac72803647ba5c9a2e92711f2576
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-09 16:57:34 +0500

    authors.js

---
+++ pages/authors.js
@@ -0,0 +1,98 @@
+// Show all users in the discover author page
+
+
+import React, { useState, useEffect, useContext } from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/author.module.css";
+import { Banner, NFTCardTwo } from "../collectionPage/collectionIndex";
+import { Brand, Title } from "../components/componentsindex";
+import FollowerTabCard from "../components/FollowerTab/FollowerTabCard/FollowerTabCard";
+import images from "../img";
+import {
+  AuthorProfileCard,
+  AuthorTaps,
+  AuthorNFTCardBox,
+} from "../authorPage/componentIndex";
+
+//IMPORT SMART CONTRACT DATA
+import { NFTMarketplaceContext } from "../Context/NFTMarketplaceContext";
+
+const authors = () => {
+  const followerArray = [
+    {
+      background: images.creatorbackground1,
+      user: images.user1,
+      seller: "7d64gf748849j47fy488444",
+    },
+    {
+      background: images.creatorbackground2,
+      user: images.user2,
+      seller: "7d64gf748849j47fy488444",
+    },
+    {
+      background: images.creatorbackground3,
+      user: images.user3,
+      seller: "7d64gf748849j47fy488444",
+    },
+    {
+      background: images.creatorbackground4,
+      user: images.user4,
+      seller: "7d64gf748849j47fy488444",
+    },
+    {
+      background: images.creatorbackground5,
+      user: images.user5,
+      seller: "7d64gf748849j47fy488444",
+    },
+    {
+      background: images.creatorbackground6,
+      user: images.user6,
+      seller: "7d64gf748849j47fy488444",
+    },
+  ];
+
+  const [collectiables, setCollectiables] = useState(true);
+  const [created, setCreated] = useState(false);
+  const [like, setLike] = useState(false);
+  const [follower, setFollower] = useState(false);
+  const [following, setFollowing] = useState(false);
+
+  //IMPORT SMART CONTRACT DATA
+  const { fetchMyNFTsOrListedNFTs, currentAccount } = useContext(
+    NFTMarketplaceContext
+  );
+
+  const [nfts, setNfts] = useState([]);
+  const [myNFTs, setMyNFTs] = useState([]);
+
+  useEffect(() => {
+    fetchMyNFTsOrListedNFTs("fetchItemsListed").then((items) => {
+      setNfts(items);
+
+      console.log(nfts);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchMyNFTsOrListedNFTs("fetchMyNFTs").then((items) => {
+      setMyNFTs(items);
+      console.log(myNFTs);
+    });
+  }, []);
+
+  return (
+    <div className={Style.author}>
+      
+      <Title 
+        heading="Popular Creators"
+       
+      />
+      
+
+      <Brand />
+    </div>
+  );
+};
+
+export default authors;
