29f05cdf6406632cac76ad2dea757c61fabc7379
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-09 17:00:14 +0500

    collection.js

---
+++ pages/collection.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/collection.module.css";
+import images from "../img";
+import {
+  Banner,
+  CollectionProfile,
+  NFTCardTwo,
+} from "../collectionPage/collectionIndex";
+import { Slider, Brand } from "../components/componentsindex";
+import Filter from "../components/Filter/Filter";
+
+const collection = () => {
+  const collectionArray = [
+    {
+      image: "img/nft-image-1.png",
+    },
+   
+  ];
+  return (
+    <div className={Style.collection}>
+      <Banner bannerImage={images.creatorbackground1} />
+      <CollectionProfile />
+      <Filter />
+  
+    </div>
+  );
+};
+
+export default collection;
