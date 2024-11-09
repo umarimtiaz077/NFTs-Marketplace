35487f4c9bc9190f23476fb17ed21ea74dc5b299
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-09 09:45:08 +0500

    about.js updated

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -61,6 +61,32 @@ const aboutus = () => {
           </div>
         </div>
 
+        <div className={Style.aboutus_box_title}>
+          <h2>⛱ Founder</h2>
+          <p>
+            We’re impartial and independent, and every day we create
+            distinctive, world-class programmes and content
+          </p>
+        </div>
+
+        <div className={Style.aboutus_box_founder}>
+          <div className={Style.aboutus_box_founder_box}>
+            {founderArray.map((el, i) => (
+              <div className={Style.aboutus_box_founder_box_img}>
+                <Image
+                  src={el.images}
+                  alt={el.name}
+                  width={500}
+                  height={500}
+                  className={Style.aboutus_box_founder_box_img_img}
+                />
+                <h3>{el.name}</h3>
+                <p>{el.position}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
         
       </div>
       <Brand />
