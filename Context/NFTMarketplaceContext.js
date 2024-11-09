7cbabe0aa2cb9d9d434930f6824b5020bb230589
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-09 17:09:09 +0500

    Check wallet context

---
+++ Context/NFTMarketplaceContext.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+//INTERNAL  IMPORT
+import {
+  NFTMarketplaceAddress,
+  NFTMarketplaceABI,
+  transferFundsAddress,
+  transferFundsABI,
+  handleNetworkSwitch,
+} from "./constants";
+
+//---FETCHING SMART CONTRACT
+const fetchContract = (signerOrProvider) =>
+  new ethers.Contract(
+    NFTMarketplaceAddress,
+    NFTMarketplaceABI,
+    signerOrProvider
+  );
+
+//---CONNECTING WITH SMART CONTRACT
+
+const connectingWithSmartContract = async () => {
+  try {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const contract = fetchContract(signer);
+    return contract;
+  } catch (error) {
+    console.log("Something went wrong while connecting with contract", error);
+  }
+};
+
+export const NFTMarketplaceContext = React.createContext();
+
+export const NFTMarketplaceProvider = ({ children }) => {
+  const titleData = "Discover, collect, and sell NFTs";
+
+  //------USESTAT
+  const [error, setError] = useState("");
+  const [openError, setOpenError] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [accountBalance, setAccountBalance] = useState("");
+  const router = useRouter();
+
+  //---CHECK IF WALLET IS CONNECTD
+
+  const checkIfWalletConnected = async () => {
+    try {
+      if (!window.ethereum)
+        return setOpenError(true), setError("Install MetaMask");
+      const network = await handleNetworkSwitch();
+
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const getBalance = await provider.getBalance(accounts[0]);
+        const bal = ethers.utils.formatEther(getBalance);
+        setAccountBalance(bal);
+        return accounts[0];
+      } else {
+        // setError("No Account Found");
+        // setOpenError(true);
+        console.log("No account");
+      }
+    } catch (error) {
+      // setError("Something wrong while connecting to wallet");
+      // setOpenError(true);
+      console.log("not connected");
+    }
+  };
+
+ 
+};
