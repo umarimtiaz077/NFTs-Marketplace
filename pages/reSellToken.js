8782837638a2eb2400ae1ab0cbd283f3801e7331
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-12 18:09:27 +0500

    resellToken Page updated

--- pages/reSellToken.js
+++ pages/reSellToken.js
@@ -38,7 +38,34 @@ const reSellToken = () => {
       console.log("Error while resell", error);
     }
   };
+  return (
+    <div className={Style.reSellToken}>
+      <div className={Style.reSellToken_box}>
+        <h1>ReSell Your Token, Set Price</h1>
+        <div className={formStyle.Form_box_input}>
+          <label htmlFor="name">Price</label>
+          <input
+            type="number"
+            min={1}
+            placeholder="reSell price"
+            className={formStyle.Form_box_input_userName}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
 
+        <div className={Style.reSellToken_box_image}>
+          {image && (
+            <Image src={image} alt="resell nft" width={400} height={400} />
+          )}
+        </div>
+
+        <div className={Style.reSellToken_box_btn}>
+          <Button btnName="Resell NFT" handleClick={() => resell()} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default reSellToken;
+
