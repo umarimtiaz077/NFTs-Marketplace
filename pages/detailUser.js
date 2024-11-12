988ed95faea3cc8b205f0df05fc907069e3d49d7
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-12 17:35:56 +0500

    detailUser page updated

--- pages/detailUser.js
+++ pages/detailUser.js
@@ -78,7 +78,42 @@ const detailUser = () => {
     });
   }, []);
 
+  return (
+    <div className={Style.author}>
+      <Banner bannerImage={images.creatorbackground2} />
+      <AuthorProfileCard currentAccount={currentAccount} />
+      <AuthorTaps
+        setCollectiables={setCollectiables}
+        setCreated={setCreated}
+        setLike={setLike}
+        setFollower={setFollower}
+        setFollowing={setFollowing}
+        currentAccount={currentAccount}
+      />
 
+      <AuthorNFTCardBox
+        collectiables={collectiables}
+        created={created}
+        like={like}
+        follower={follower}
+        following={following}
+        nfts={nfts}
+        myNFTS={myNFTs}
+      />
+      <Title
+        heading="Popular Creators"
+        paragraph="Click on music icon and enjoy NTF music or audio
+"
+      />
+      <div className={Style.author_box}>
+        {followerArray.map((el, i) => (
+          <FollowerTabCard i={i} el={el} />
+        ))}
+      </div>
+
+      <Brand />
+    </div>
+  );
 };
 
 export default detailUser;
