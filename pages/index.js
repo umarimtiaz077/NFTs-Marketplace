803d9adc5aa580bef94293ec1d828e89d29b7982
Syed Umar Imtiaz <callmeumar077@gmail.com>
2024-11-12 17:42:10 +0500

    index.js page updated

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,53 @@ const Home = () => {
   const creators = getTopCreators(nfts);
   // console.log(creators);
 
-  
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+      {/* <Title
+        heading="Audio Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <AudioLive /> */}
+      {creators.length == 0 ? (
+        <Loader />
+      ) : (
+        <FollowerTab TopCreator={creators} />
+      )}
+
+      {/* <Slider /> */}
+      <Collection />
+      <div className={Style.followerTab_member}>
+        <div className={Style.followerTab_member_box}>
+          <a href="collectionsPage">Show me more</a>
+        </div>
+      </div>
+
+  <div className={Style.centeredContainer}>
+  <Title
+    heading="Featured NFTs"
+  />
+</div>
+
+      {nfts.length == 0 ? <Loader /> : <NFTCard NFTData={nfts} />}
+      <div className={Style.followerTab_member}>
+        <div className={Style.followerTab_member_box}>
+          <a href="searchPage">Show me more</a>
+        </div>
+      </div>
+
+      {/* <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category /> */}
+      <Subscribe />
+      {/* <Brand /> */}
+      {/* <Video /> */}
+    </div>
+  );
 };
 
 export default Home;
