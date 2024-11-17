f3d823b96d32b2a1c5c8a550ad5d3cb513d45fa8
students apna college <student@apnacollege.in>
2024-11-17 10:58:27 +0500

    added slice of 3 to show in frontend

--- components/FollowerTab/FollowerTab.jsx
+++ components/FollowerTab/FollowerTab.jsx
@@ -11,84 +11,8 @@ import Style from "./FollowerTab.module.css";
 import FollowerTabCard from "./FollowerTabCard/FollowerTabCard";
 import images from "../../img";
 
-const FollowerTab = ({ TopCreator }) => {
+const FollowerTab = ({ isslice, TopCreator }) => {
   const router = useRouter();
-
-  const FollowingArray = [
-    {
-      background: images.creatorbackground3,
-      user: images.user3,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground4,
-      user: images.user4,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground5,
-      user: images.user5,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground6,
-      user: images.user6,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground1,
-      user: images.user1,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground2,
-      user: images.user2,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-  ];
-  
-  const NewsArray = [
-    {
-      background: images.creatorbackground1,
-      user: images.user1,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground2,
-      user: images.user2,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground3,
-      user: images.user3,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground4,
-      user: images.user4,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground5,
-      user: images.user5,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground6,
-      user: images.user6,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground7,
-      user: images.user7,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-    {
-      background: images.creatorbackground8,
-      user: images.user8,
-      seller: "7200d8d8390d9993ujdc93900399djj277x",
-    },
-  ];
 
   const [popular, setPopular] = useState(true);
   const [following, setFollowing] = useState(false);
@@ -121,6 +45,9 @@ const FollowerTab = ({ TopCreator }) => {
     router.push(`/author?seller=${seller}`); // Navigate to author.js with seller ID in query
   };
 
+  console.log(popular, following, news, TopCreator);
+  
+
   return (
     <div className={Style.followerTab}>
       <div className={Style.followerTab_title}>
@@ -140,17 +67,38 @@ const FollowerTab = ({ TopCreator }) => {
         </div>
       </div>
 
-      {popular && (
-        <div className={Style.followerTab_box}>
-          {TopCreator.map((el, i) => (
-            <div key={i + 1} onClick={() => redirectToDetailPage(el.seller)}>
-              <FollowerTabCard i={i} el={el} />
+      {isslice ? (
+        <>
+          {popular && (
+            <div className={Style.followerTab_box}>
+              {TopCreator.slice(0, 3).map((el, i) => (
+                <div
+                  key={i + 1}
+                  onClick={() => redirectToDetailPage(el.seller)}
+                >
+                  <FollowerTabCard i={i} el={el} />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          )}
+        </>
+      ) : (
+        <>
+          {popular && (
+            <div className={Style.followerTab_box}>
+              {TopCreator.map((el, i) => (
+                <div
+                  key={i + 1}
+                  onClick={() => redirectToDetailPage(el.seller)}
+                >
+                  <FollowerTabCard i={i} el={el} />
+                </div>
+              ))}
+            </div>
+          )}
+        </>
       )}
-
-      {following && (
+      {/* {following && (
         <div className={Style.followerTab_box}>
           {FollowingArray.map((el, i) => (
             <div key={i + 1} onClick={() => redirectToDetailPage(el.seller)}>
@@ -168,7 +116,7 @@ const FollowerTab = ({ TopCreator }) => {
             </div>
           ))}
         </div>
-      )}
+      )} */}
 
       <div className={Style.followerTab_member}>
         <div className={Style.followerTab_member_box}>
